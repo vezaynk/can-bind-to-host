0fd03066e2fb2ae67fcc3ab36f2f98a05fafbcb9
Slava Knyazev <knyaze@amazon.com>
2022-07-14 04:42:47 +0000

    Add eslint

--- src/bin/can-bind-to-host.ts
+++ src/bin/can-bind-to-host.ts
@@ -1,15 +1,15 @@
 #! /usr/bin/env node
-import canBindToHost from "..";
+import canBindToHost from '..';
 
 const args = process.argv.slice(2);
 
-const [host="0.0.0.0", port="0"] = args;
-canBindToHost(host, +port).then(bindable => {
-    if (bindable) {
-        console.log(`${host}:${port} is bindable`);
-        process.exit(0);
-    } else {
-        console.log(`${host}:${port} is not bindable`);
-        process.exit(1);
-    }
-})
+const [host = '0.0.0.0', port = '0'] = args;
+canBindToHost(host, +port).then((bindable) => {
+  if (bindable) {
+    console.log(`${host}:${port} is bindable`);
+    process.exit(0);
+  } else {
+    console.log(`${host}:${port} is not bindable`);
+    process.exit(1);
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import net from 'net';
 
-const canBindToHost = (host: string = "0.0.0.0", port: number = 0) => {
+const canBindToHost = (host: string = '0.0.0.0', port: number = 0) => {
   let server: net.Server | null = null;
   return new Promise<boolean>((res) => {
-    if (port >= 0 && port <= 65_535)
+    if (port >= 0 && port <= 65_535) {
       server = net
         .createServer()
         .listen({
@@ -15,8 +15,7 @@ const canBindToHost = (host: string = "0.0.0.0", port: number = 0) => {
           server?.close();
           res(true);
         });
-      else
-        res(false);
+    } else res(false);
   });
 };
 
