0b3329c20907161ce4c890df9bae137fcae3416d
Slava Knyazev <knyaze@amazon.com>
2022-07-14 16:26:06 +0000

    Fix lint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import net from 'net';
 
-const canBindToHost = (host: string = '0.0.0.0', port: number = 0) => {
-  return new Promise<boolean>((res) => {
-    if (port >= 0 && port <= 65_535) {
-      const server = net
-        .createServer()
-        .listen({
-          host,
-          port,
-        })
-        .addListener('error', () => res(false))
-        .addListener('listening', () => {
-          server.close();
-          res(true);
-        });
-    } else res(false);
-  });
-};
+const canBindToHost = (host: string = '0.0.0.0', port: number = 0) => new Promise<boolean>((res) => {
+  if (port >= 0 && port <= 65_535) {
+    const server = net
+      .createServer()
+      .listen({
+        host,
+        port,
+      })
+      .addListener('error', () => res(false))
+      .addListener('listening', () => {
+        server.close();
+        res(true);
+      });
+  } else res(false);
+});
 
 export default canBindToHost;
 export { canBindToHost };
