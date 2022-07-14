c3b1b3cba4d306238b31d2c0c6c4304c1b50c711
Slava Knyazev <knyaze@amazon.com>
2022-07-14 16:18:27 +0000

    Add new action

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import net from 'net';
 
 const canBindToHost = (host: string = '0.0.0.0', port: number = 0) => {
-  let server: net.Server | null = null;
   return new Promise<boolean>((res) => {
     if (port >= 0 && port <= 65_535) {
-      server = net
+      const server = net
         .createServer()
         .listen({
           host,
-          port: 0,
+          port,
         })
         .addListener('error', () => res(false))
         .addListener('listening', () => {
-          server?.close();
+          server.close();
           res(true);
         });
     } else res(false);
